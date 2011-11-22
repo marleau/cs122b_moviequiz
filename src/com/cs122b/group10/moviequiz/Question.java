6cc86b21df011e318478272cbf86031ef31088a9
TheNeisius <abrageddon@gmail.com>
2011-11-21 22:11:08 -0800

    user interface setup; quiz timer works properly for pause and save state instances

--- src/com/cs122b/group10/moviequiz/Question.java
+++ src/com/cs122b/group10/moviequiz/Question.java
@@ -1,19 +1,22 @@
 package com.cs122b.group10.moviequiz;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.*;
 import android.widget.TextView;
 
 public class Question extends Activity {
 	private static final long duration = 180000;//3 minutes
 	
-	//Timer
+	//Setup Timer
 	private TextView timerText;
 	private Handler mHandler = new Handler();
-	private long mStart;
-	private Runnable updateTask = new Runnable() {	
+	private long mStartTime;
+	private Runnable quizTimer = new Runnable() {	
 		public void run() {
 			long now = SystemClock.uptimeMillis();
-			long elapsed = duration - (now - mStart);
+			long elapsed = duration - (now - mStartTime);
 			
 			if (elapsed > 0) {
 				int seconds = (int) (elapsed / 1000);
@@ -31,24 +34,40 @@ public class Question extends Activity {
 			else {
 				mHandler.removeCallbacks(this);
 				//TODO Game Over -- tally results and display
-				
+				AlertDialog alert = builder.create();
+				alert.show();
 				finish();
 			}
 		}
 	};
+
+	private long pausedTime;
+	
+	AlertDialog.Builder builder;
 	
     @Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question);
         
+        //end game dialog
+        builder = new AlertDialog.Builder(this);
+        builder.setMessage("Game Over!")
+               .setCancelable(false)
+               .setPositiveButton("Finally!", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                   }
+               });
+
 
         //initalize timer
-
         timerText = (TextView)this.findViewById(R.id.timerText);
-        mStart = SystemClock.uptimeMillis();
-        mHandler.post(updateTask);
-        
+        pausedTime = 0;
+        //Uptime and not current time.... weird; used for: mHandler.postAtTime(this, now + 1000);
+        mStartTime = SystemClock.uptimeMillis();
+        mHandler.post(quizTimer);
+
         
         //create question
         /*	Who directed the movie X?
@@ -68,18 +87,77 @@ public class Question extends Activity {
         
     }
     
+    public void onBackPressed() {
+    	//Alert user that they are leaving the quiz
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("End current quiz session?")
+               .setCancelable(false)
+               .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                   }
+               })
+               .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                   }
+               });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
 
     @Override
     protected void onPause() {
         super.onPause();
+        //When the application is paused, the timer should also be paused and it shouldn't count down.
         //TODO stop timer
 
+		mHandler.removeCallbacks(quizTimer);
+		pausedTime = SystemClock.uptimeMillis();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         //TODO start timer
+        if (pausedTime != 0){
+        	mStartTime += SystemClock.uptimeMillis() - pausedTime;
+        }
+        mHandler.post(quizTimer);
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        /*	When the user pauses the application, it can be killed by the system in extremely low memory situations.
+         *  Your program should save everything (e.g., current question, timer, and statistics) properly and be able
+         *  to resume later. Take a look at  this page to learn more about life cycles in an Android program. You can 
+         *  override  onSaveInstanceState() method to save the current state. Please refer to  this example. A good 
+         *  way to test your application's ability to restore its state is to simply rotate the device so that the 
+         *  screen orientation changes. (You can simulate rotation on your emulator by pressing Ctrl-F11.) When the 
+         *  screen orientation changes, the system destroys and recreates the activity in order to apply alternative 
+         *  resources that might be available for the new orientation.*/ 
+        //TODO save all values
+
+        outState.putLong("mStartTime", mStartTime);
+        outState.putLong("pausedTime", SystemClock.uptimeMillis());
+        
+
+        super.onSaveInstanceState(outState);
+    }
+    
+
+    @Override
+    protected void onRestoreInstanceState(Bundle inState) {
+    	//TODO load all values and adjust clock like on resume
+    	
+    	
+    	mStartTime = inState.getLong("mStartTime");
+        if (pausedTime != 0){
+        	mStartTime += SystemClock.uptimeMillis() - inState.getLong("pausedTime");
+        }
+        mHandler.post(quizTimer);
+        
+        super.onRestoreInstanceState(inState);
     }
 }
--- src/com/cs122b/group10/moviequiz/Statistics.java
+++ src/com/cs122b/group10/moviequiz/Statistics.java
@@ -4,8 +4,13 @@ import android.os.*;
 import android.view.View;
 import android.widget.Button;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class Statistics extends Activity {
+	
+	private AlertDialog.Builder builder;
+	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistics);
@@ -17,6 +22,33 @@ public class Statistics extends Activity {
 
             public void onClick(View view) {
 				finish();
+            }
+
+        });
+        
+        Button resetStats = (Button) findViewById(R.id.resetStatsButton);
+        
+        //build reset stats dialog
+        builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to reset statistics?")
+	       .setCancelable(false)
+	       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+	           public void onClick(DialogInterface dialog, int id) {
+	                //TODO RESET STATS
+	           }
+	       })
+	       .setNegativeButton("No", new DialogInterface.OnClickListener() {
+	           public void onClick(DialogInterface dialog, int id) {
+	                dialog.cancel();
+	           }
+	       });
+        
+        resetStats.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+				//ask for confirmation and reset stats
+            	AlertDialog alert = builder.create();
+                alert.show();
             }
 
         });
