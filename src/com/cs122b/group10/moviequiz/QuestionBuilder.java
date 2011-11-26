4fc0aee9e71ca5c34ac558deb415f0e422339ac4
Arielle <thetomato@gmail.com>
2011-11-25 21:22:07 -0800

    implement question 5

--- src/com/cs122b/group10/moviequiz/QuestionBuilder.java
+++ src/com/cs122b/group10/moviequiz/QuestionBuilder.java
@@ -16,7 +16,7 @@ import android.database.Cursor;
 
 public class QuestionBuilder {
 
-    private static final int NUMBER_OF_TYPES = 4;//Types of questions
+    private static final int NUMBER_OF_TYPES = 8;//Types of questions
     private DBAdapter db;
     private String[] answers;
     private String correct;
@@ -33,8 +33,8 @@ public class QuestionBuilder {
         for (int i = 0; i < 4; i++) { answers[i] = ""; }
         correct = "";
         question = "";
-        int type = rand.nextInt(NUMBER_OF_TYPES)+1;
-        //int type = 4;
+        //int type = rand.nextInt(NUMBER_OF_TYPES)+1;
+        int type = 5;
         buildQuestion(type);
     }
 
@@ -113,8 +113,8 @@ public class QuestionBuilder {
     // 3. Which star (was/was not) in the movie X?
     private void buildStarInOrNotOneMovie() {
         final String stars_movies_join = "movies, stars, stars_in_movies WHERE stars_in_movies.movie_id = movies.id AND stars_in_movies.star_id = stars.id";
-        final String star_in_query = "SELECT DISTINCT stars.name FROM " + stars_movies_join + " AND movies.id = ? ORDER BY RANDOM()";
-        final String star_out_query = "SELECT DISTINCT stars.name FROM " + stars_movies_join + " AND movies.id != ? ORDER BY RANDOM()";
+        final String star_in_query = "SELECT DISTINCT stars.name FROM " + stars_movies_join + " AND movies.id = ? ORDER BY RANDOM() LIMIT 5";
+        final String star_out_query = "SELECT DISTINCT stars.name FROM " + stars_movies_join + " AND movies.id != ? ORDER BY RANDOM() LIMIT 5";
         final String movie_query = "SELECT title, id FROM movies ORDER BY RANDOM() LIMIT 1";
         final int state = rand.nextInt(2); // 0: is not in movie, 1: is no movie
 
@@ -137,7 +137,7 @@ public class QuestionBuilder {
     // 4. In which movie the stars X and Y appear together?
     private void buildMovieWithTwoStars() {
 
-        final String two_stars_query = "SELECT DISTINCT movies.id, movies.title, stars1.id, stars1.name, stars2.id, stars2.name FROM movies, stars AS stars1, stars AS stars2, stars_in_movies AS stars_in_movies1, stars_in_movies AS stars_in_movies2 WHERE stars_in_movies1.movie_id = movies.id AND stars_in_movies2.movie_id = movies.id AND stars_in_movies1.star_id = stars1.id AND stars_in_movies2.star_id = stars2.id AND stars1.id != stars2.id ORDER BY RANDOM() LIMIT 1";
+        final String two_stars_query = "SELECT movies.id, movies.title, stars1.id, stars1.name, stars2.id, stars2.name FROM movies, stars AS stars1, stars AS stars2, stars_in_movies AS stars_in_movies1, stars_in_movies AS stars_in_movies2 WHERE stars_in_movies1.movie_id = movies.id AND stars_in_movies2.movie_id = movies.id AND stars_in_movies1.star_id = stars1.id AND stars_in_movies2.star_id = stars2.id AND stars1.id != stars2.id ORDER BY RANDOM() LIMIT 1";
         final Cursor two_stars_cursor = db.executeQuery(two_stars_query);
         two_stars_cursor.moveToFirst();
         final int movie_id = two_stars_cursor.getInt(0);
@@ -151,13 +151,30 @@ public class QuestionBuilder {
         correct = cleanAnswer(movie);
         question = "In which movie, did "+cleanAnswer(star1)+" and "+cleanAnswer(star2)+" appear together?";
 
-        final String wrong_query = "SELECT DISTINCT movies.title FROM movies, stars_in_movies AS stars1, stars_in_movies AS stars2  WHERE stars1.movie_id = movies.id AND stars2.movie_id = movies.id AND movies.id != ? AND stars1.star_id != ? AND stars2.star_id != ? ORDER BY RANDOM()";
+        final String wrong_query = "SELECT DISTINCT movies.title FROM movies, stars_in_movies AS stars1, stars_in_movies AS stars2  WHERE stars1.movie_id = movies.id AND stars2.movie_id = movies.id AND movies.id != ? AND stars1.star_id != ? AND stars2.star_id != ? ORDER BY RANDOM() LIMIT 5";
         populateWrongAnswers(true, db.executeQuery(wrong_query, Integer.toString(movie_id), Integer.toString(star1_id), Integer.toString(star2_id)));
     }
 
     // 5. Who directed/did not direct the star X?
     private void buildWhoDirectedOrNotStar() {
-        question = "Who directed/did not direct the star X?";
+        final int state = rand.nextInt(2); // 0: did direct, 1: did not direct
+        final String star_query = "SELECT DISTINCT stars.name, stars.id FROM stars ORDER BY RANDOM() LIMIT 1";
+        final Cursor star_cursor = db.executeQuery(star_query);
+        star_cursor.moveToFirst();
+        final String star = star_cursor.getString(0);
+        final int star_id = star_cursor.getInt(1);
+        final String did_direct_query = "SELECT DISTINCT movies.director FROM movies, stars, stars_in_movies WHERE stars_in_movies.movie_id = movies.id AND stars_in_movies.star_id = stars.id AND stars.id = ? ORDER BY RANDOM() LIMIT 5";
+        final String not_direct_query = "SELECT DISTINCT movies.director FROM movies, stars, stars_in_movies WHERE stars_in_movies.movie_id = movies.id AND stars_in_movies.star_id = stars.id AND stars.id != ? ORDER BY RANDOM() LIMIT 5";
+
+        if (state == 0) {
+            question = "Who did not direct "+cleanAnswer(star)+"?";
+            populateWrongAnswers(true, db.executeQuery(did_direct_query, Integer.toString(star_id)));
+            populateCorrectAnswer(true, db.executeQuery(not_direct_query, Integer.toString(star_id)));
+        } else {
+            question = "Who directed "+cleanAnswer(star)+"?";
+            populateWrongAnswers(true, db.executeQuery(not_direct_query, Integer.toString(star_id)));
+            populateCorrectAnswer(true, db.executeQuery(did_direct_query, Integer.toString(star_id)));
+        }
     }
 
     // 6. Which star appears in both movies X and Y?
