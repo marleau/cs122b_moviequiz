6926d0aa64fb050c4b3d321d3470468d91a040bd
Arielle <thetomato@gmail.com>
2011-11-30 19:02:52 -0800

    add zip file to turn it

--- README.txt
+++ README.txt
@@ -1,1 +1,17 @@
 Package name: com.cs122b.group10.moviequiz
+
+cd /usr/local/Cellar/android-sdk/r15/tools
+./emulator -avd cs122b
+adb kill-server
+adb start-server
+
+To uninstall:
+
+adb shell
+cd data/app
+ls
+rm
+
+To install:
+
+dab install path/to/.apk/file
---
+++ gen/R.java.d
@@ -0,0 +1,9 @@
+/Users/arielle/archive/UCI/2011-2012_fall/cs122b/project5/MovieQuiz/gen/com/cs122b/group10/moviequiz/R.java \
+ : /Users/arielle/archive/UCI/2011-2012_fall/cs122b/project5/MovieQuiz/res/drawable-hdpi/ic_launcher.png \
+/Users/arielle/archive/UCI/2011-2012_fall/cs122b/project5/MovieQuiz/res/drawable-ldpi/ic_launcher.png \
+/Users/arielle/archive/UCI/2011-2012_fall/cs122b/project5/MovieQuiz/res/drawable-mdpi/ic_launcher.png \
+/Users/arielle/archive/UCI/2011-2012_fall/cs122b/project5/MovieQuiz/res/layout/main.xml \
+/Users/arielle/archive/UCI/2011-2012_fall/cs122b/project5/MovieQuiz/res/layout/question.xml \
+/Users/arielle/archive/UCI/2011-2012_fall/cs122b/project5/MovieQuiz/res/layout/statistics.xml \
+/Users/arielle/archive/UCI/2011-2012_fall/cs122b/project5/MovieQuiz/res/values/strings.xml \
+/Users/arielle/archive/UCI/2011-2012_fall/cs122b/project5/MovieQuiz/AndroidManifest.xml \
